cca9e9dc8485004541818a87bab0e156c7d08a38
Roxy Osorio <roxy.osorio@moo.com>
2023-06-27 20:50:17 -0400

    modified server.js, & seeds

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,19 +1,27 @@
 const { Schema, model } = require('mongoose');
 
-const profileSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  skills: [
-    {
+const profileSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    skills: {
       type: String,
       trim: true,
     },
-  ],
-});
+    rate: {
+      type: Number,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    }
+  }
+);
 
 const Profile = model('Profile', profileSchema);
 
