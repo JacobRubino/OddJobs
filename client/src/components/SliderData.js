db2826330fa97f5399c3dcb0a996620d4b686ea0
Jacob Rubino <jacobrubino@gmail.com>
2023-06-29 21:28:46 -0400

    front page carousel

---
+++ client/src/components/SliderData.js
@@ -0,0 +1,27 @@
+const SliderData = [
+  {
+    image: 'https://images.pexels.com/photos/1249610/pexels-photo-1249610.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+  {
+    image:'https://images.pexels.com/photos/4325328/pexels-photo-4325328.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+  {
+    image:'https://images.pexels.com/photos/4239031/pexels-photo-4239031.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+  {
+    image:'https://images.pexels.com/photos/8005368/pexels-photo-8005368.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+  {
+    image:'https://images.pexels.com/photos/7464701/pexels-photo-7464701.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+  {
+    image:'https://images.pexels.com/photos/1917849/pexels-photo-1917849.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+  {
+    image:'https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+  {
+    image:'https://images.pexels.com/photos/7210524/pexels-photo-7210524.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+]
+module.exports = SliderData;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,52 @@
-import React from 'react';
-import Carousel from '../components/carousel';
+import React from 'react'
+import SliderData from '../components/SliderData'
+import { FaArrowAltCircleRight } from 'react-icons/fa'
+import { FaArrowAltCircleLeft } from 'react-icons/fa'
+import './home.css'
+import { useState, useEffect } from'react';
 
-const Home = () => {
-  const images = [];
-  const imageTexts = [];
+const ImageSlider = ({ slides }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((prevImage) => (prevImage + 1) % slides.length);
+    }, 6000);
+
+    return () => clearInterval(interval);
+  }, [slides]);
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
 
   return (
-    <div>
-      <h1>Home Page</h1>
-      <Carousel images={images} imageTexts={imageTexts} />
-    </div>
+    <section className='slider'>
+      <FaArrowAltCircleLeft className='arrow-left' onClick={prevSlide} />
+      <FaArrowAltCircleRight className='arrow-right' onClick={nextSlide} />
+      {SliderData.map((slide, index) => {
+        return (
+          <div
+            className={index === current ? 'slide active' : 'slide'}
+            key={index}
+          >
+            {index === current && (
+              <img src={slide.image} alt='travel image' className='image' />
+            )}
+          </div>
+        );
+      })}
+    </section>
   );
 };
 
-export default Home;
+export default ImageSlider;
