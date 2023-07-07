6de766f36ba546f90a5c1902f95d38e5b85638f6
Roxy Osorio <roxy.osorio@moo.com>
2023-07-06 21:08:14 -0400

    added pre-save hook to has passwords in profile.js

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const profileSchema = new Schema(
   {
@@ -19,10 +20,32 @@ const profileSchema = new Schema(
     location: {
       type: String,
       required: true
-    }
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  }
+);
+
+profileSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
   }
 
-);
+  next();
+});
+
+profileSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const Profile = model('Profile', profileSchema);
 
