afb9a2a1c708ea39ac0574dd17e7d5e2e1530253
Jacob Rubino <jacobrubino@gmail.com>
2023-07-10 15:30:54 -0400

    created the layouts for stripe

---
+++ client/src/pages/TransactionCancelled.js
@@ -0,0 +1,14 @@
+import React , { useState, useEffect} from 'react';
+
+const TransactionCancelled = () => {
+  return (
+    <section>
+      <p>
+        Transaction has been cancelled.
+      </p>
+      
+    </section>
+  )
+}
+
+export default TransactionCancelled;
