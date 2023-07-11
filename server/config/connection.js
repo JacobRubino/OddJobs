1f175f6ff4a26a87b3e77d4b72fca578e9b375ee
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-07-11 15:02:47 -0400

    heroku updates to mongodb atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://evansmiller:<FinalProject>@oddjobs.wwyzdrs.mongodb.net/?retryWrites=true&w=majority'
+  process.env.MONGODB_URI || 'mongodb+srv://evansmiller:FinalProject@oddjobs.wwyzdrs.mongodb.net/?retryWrites=true&w=majority'
 );
 
 module.exports = mongoose.connection;
