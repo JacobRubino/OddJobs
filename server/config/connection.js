6029424d5fff3dc4e19ebfce5d9810ca7b4981f3
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-07-10 20:08:01 -0400

    heroku updates

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 mongoose.connect(
-  process.env.MONGODB_URI || ATLAS_URI
+  process.env.MONGODB_URI || ATLAS
 );
 
 module.exports = mongoose.connection;
