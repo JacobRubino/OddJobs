b7328cbae852ca95a40fe2f8f868f3c7886da22d
Roxy Osorio <roxy.osorio@moo.com>
2023-07-13 11:01:41 -0400

    about me paragraphs and replaced image

--- client/src/pages/Aboutme.js
+++ client/src/pages/Aboutme.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import image1 from '../images/aboutme1.jpg';
+import image3 from '../images/aboutme3.jpg';
+import image4 from '../images/aboutme4.jpg';
+import image5 from '../images/girlSmilingOnCouch.jpg';
 import { FaGithub } from 'react-icons/fa';
 import { ImHeart } from 'react-icons/im';
 import './aboutme.css';
 
 const AboutMe = () => {
   return (
-    <div>
+<div>
       <div className="header">
         <h1>Repairs, Maintenance & More</h1>
 
@@ -15,82 +19,62 @@ const AboutMe = () => {
               <a href="#job-opportunities">Life Made Easier</a>
             </li>
             <li>
-              <a href="#working-conditions">Job Opportunities</a>
+              <a href="#working-conditions">Where Do I Begin?</a>
             </li>
             <li>
-              <a href="#community-support">Community Support</a>
-            </li>
-            <li>
-              <a href="#empowerment">Empowerment</a>
+              <a href="#community-support">Job Opportunities</a>
             </li>
           </ul>
         </div>
-        <br />
-        <p style={{ fontSize: '1.5rem', color: 'white' }}>
-          Welcome to OddJobs, your one-stop destination for all your task-related needs! We are a dynamic and innovative
-          company that connects individuals and businesses with skilled task-doers, helping you save time, enhance
-          productivity and focus on what truly matters.
-        </p>
+        <br></br>
+
       </div>
-
-      {/* <h4 style={{ color: 'navy' }}>Section Title</h4> */}
-
+      <h4 style={{ color: 'navy' }}>Welcome to OddJobs, your one-stop destination for all your task-related needs! We are a dynamic and innovative company that connects individuals and businesses with skilled task-doers, helping you save time, enhance productivity and focus on what truly matters. </h4>
       <div className="hero"></div>
-     
-
-      <div className="container">
-      <div id="job-opportunities" className="card card-aboutme">
+      <div className="content">
+        <div className="job-opportunities">
+          <img src={image5} className="float-left resized-image" alt="Job Opportunities" />
           <h2>Life Made Easier</h2>
-          <p>
-            We are delighted to have you join our network of skilled professionals, and we are excited about the
-            opportunities that lie ahead for your business. Our mission is to provide a platform where you can showcase
-            your skills, gain visibility, and find new clients easily. We are dedicated to supporting your success and
-            helping you thrive in your handyman career. We are delighted to have you join our network of skilled
-            professionals, and we are excited about the opportunities that lie ahead for your handyman business.
+          <p>At OddJobs, we understand that life can get overwhelming and that juggling multiple tasks on your to-do list can be challenging. That's why we've created a platform that gives you a network of skilled individuals who are ready to handle a wide variety of tasks with their expertise. At OddJobs, you can experience the true meaning of relaxation, knowing that your tasks are being handled efficiently and professionally. Say goodbye to stress and overwhelm and say hello to more time for the things you love. Let us help you make your life simpler, one task at a time!
           </p>
         </div>
+        <div id="working-conditions" className="working-conditions">
+          <img src={image1} className="float-right resized-image" alt="Working Conditions" />
+          <br />
+          <br />
+          <h2>Where Do I Begin?</h2>
+          <p>
+          With OddJobs, the process is simple. Just browse our platform, select the task you need assistance with, and choose a task-doer who fits your requirements. You have the freedom to negotiate the terms, timeline, and pricing directly with the task-doer, ensuring that you have full control over your tasks. Join us at Odd Jobs and experience the freedom and peace of mind that comes with delegating your tasks to capable professionals. 
 
-        <div id="working-conditions" className="card card-aboutme">
+          </p>
+        </div>
+        <div id="community-support" className="community-support">
+          <img src={image3} className="float-left resized-image" alt="Community Support" />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
           <h2>Job Opportunities</h2>
           <p>
-            As a member of the OddJobs network, you can expect the following benefits:
-            <ul>
-              <li>Increased Visibility: We will actively promote your handyman services through our online platform and marketing channels.</li>
-              <li>User-Friendly Platform: Our user-friendly website and mobile app make it easy for customers to discover and book your services.</li>
-              <li>Reliable and friendly Customers</li>
-              <li>Flexible Schedule</li>
-            </ul>
-          </p>
-        </div>
-
-        <div id="community-support" className="card card-aboutme">
-          <h2>Community Support</h2>
-          <p>
-            We believe that by partnering with OddJobs, you will have the tools and support necessary to grow your
-            handyman business successfully. We are passionate about connecting handymen like yourself with clients who
-            appreciate your expertise and professionalism.
-          </p>
-        </div>
-
-        <div id="empowerment" className="card card-aboutme">
-          <h2>Empowerment</h2>
-          <p>
-            We are thrilled to have you as part of the OddJobs community and look forward to witnessing your continued
-            success. If you have any questions or need further assistance, please do not hesitate to reach out to our
-            support team by filling the form below.
+          We are delighted to have you join our network of skilled professionals, and we are excited about the opportunities that lie ahead for your business. Our mission is to provide a platform where you can showcase your skills, gain visibility, and find new clients easily with the benefits of a flexible schedule. We are dedicated to supporting your success and helping you thrive in your career. We are here to support you every step of the way. Our dedicated support team is available to assist you with any inquiries or concerns you may have.
           </p>
         </div>
       </div>
-
+      <div className="benefits">
+        <div className="benefit-empowerment">
+          <h3> Community Empowerment</h3>
+          <img src={image4} className="resized-image" alt="Empowerment" />
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        {/* ... */}
+      </div>
       <div className="footer">
-        <h2>
-          Made with <ImHeart /> by <FaGithub /> <a href="https://github.com/EvanSterlingMiller">Evan Sterling Miller</a>,{' '}
-          <a href="https://github.com/meetsudarshan"> Sudarshan Samaddar</a>,{' '}
-          <a href="https://github.com/JacobRubino">Jacob Rubino</a>,{' '}
-          <a href="https://github.com/chartpro-com">Matt Fleming</a> & <a href="https://github.com/rosebudroro">Roxy Osorio</a>
-        </h2>
-
-        <p>&copy; {new Date().getFullYear()} OddJobs Community</p>
+        <h2>Made with < ImHeart /> by <FaGithub /> <a href="https://github.com/EvanSterlingMiller">Evan Sterling Miller</a>, <a href="https://github.com/meetsudarshan"> Sudarshan Samaddar</a>, <a href="https://github.com/JacobRubino">Jacob Rubino</a>, <a href="https://github.com/chartpro-com">Matt Fleming</a> & <a href="https://github.com/rosebudroro">Roxy Osorio</a></h2>
+        
+        <p>&copy; {new Date().getFullYear()} DailyWageEarners Community</p>
       </div>
     </div>
   );
